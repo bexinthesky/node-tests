5d1e215895819c2585585c525c93391215e5d030
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-06 14:47:35 -0700

    used describe() to organize tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,37 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
+describe('Server', () => {
+
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+    describe('GET /users', () => {
+        // Make a new test, assert 200, assert that you exist in users array
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Usagi',
+                        age: 21
+                    });
+                })
+                .end(done);
+        });
+    });
 });
 
-// Make a new test
-// assert 200
-// assert that you exist in users array
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Usagi',
-                age: 21
-            });
-        })
-        .end(done);
-});
+
+
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,40 +2,41 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
+
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+
+            expect(res).toBe(44).toBeA('number');
+        });
+    });
     
-    expect(res).toBe(44).toBeA('number');
-    // using expect instead of throwing errors manually ↓↓↓
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but got ${res}.`);
-    // }
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
     });
-});
 
-it('should square a number', () => {
-    var square = utils.square(9);
+    it('should square a number', () => {
+        var square = utils.square(9);
 
-    expect(square).toBe(81).toBeA('number');
-    // using expect instead of throwing errors manually ↓↓↓
-    // if (square !== 81) {
-    //     throw new Error(`Expected 81, but got ${square}.`);
-    // }
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).toBe(25).toBeA('number');
-        done();
+        expect(square).toBe(81).toBeA('number');
     });
+
+    it('should async square a number', (done) => {
+        utils.asyncSquare(5, (res) => {
+            expect(res).toBe(25).toBeA('number');
+            done();
+        });
+    });
+
 });
 
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
 it('should set firstName and lastName', () => {
     var user = {location: 'Pirates Swoop', age: 27};
     var res = utils.setName(user, 'Rebecca Lioness');
@@ -46,6 +47,13 @@ it('should set firstName and lastName', () => {
         lastName: 'Lioness'
     });
 });
+
+// throwing errors manually ↓↓↓
+// it('should add two numbers', () => {
+//     if (res !== 44) {
+//         throw new Error(`Expected 44, but got ${res}.`);
+//     }
+// });
 
 // playing with expect assertions ↓↓↓
 // it('should expect some values', () => {
