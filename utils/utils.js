0dfb82d13de73e594f75df37caf60b6a9665e685
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-06 12:44:18 -0700

    added expect library and used assertions to test code

--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,13 @@ module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => x * x;
 
+module.exports.setName = (user, fullName) => {
+    var names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+};
+
 // arrow function long version
 // module.exports.add = (a, b) => {
 //     return a + b;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,48 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
     
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    expect(res).toBe(44).toBeA('number');
+    // using expect instead of throwing errors manually ↓↓↓
+    // if (res !== 44) {
+    //     throw new Error(`Expected 44, but got ${res}.`);
+    // }
 });
 
 it('should square a number', () => {
     var square = utils.square(9);
 
-    if (square !== 81) {
-        throw new Error(`Expected 81, but got ${square}.`);
-    }
+    expect(square).toBe(81).toBeA('number');
+    // using expect instead of throwing errors manually ↓↓↓
+    // if (square !== 81) {
+    //     throw new Error(`Expected 81, but got ${square}.`);
+    // }
 });
+
+it('should set firstName and lastName', () => {
+    var user = {location: 'Pirates Swoop', age: 27};
+    var res = utils.setName(user, 'Rebecca Lioness');
+
+    // expect(user).toEqual(res);
+    expect(res).toInclude({
+        firstName: 'Rebecca',
+        lastName: 'Lioness'
+    });
+});
+
+// playing with expect assertions ↓↓↓
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: 'rebecca'}).toNotEqual({name: 'Rebecca'});
+//     // expect([2,3,4]).toExclude(1);
+//     expect({
+//         name: 'Rebecca',
+//         age: 27,
+//         location: 'place'
+//     }).toExclude({
+//         age: 23
+//     })
+// });
