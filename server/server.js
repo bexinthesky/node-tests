d074d35b8982b69a90ef2d92bb7811d95d9676e2
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-06 14:24:27 -0700

    added supertest, used it to test express applications with expect assertions

--- server/server.js
+++ server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
+});
+
+// GET /users
+// Give users a name prop and an age prop
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Usagi',
+        age: 21
+    }, {
+        name: 'Mamoru',
+        age: 24
+    }, {
+        name: 'Chibiusa',
+        age: 11
+    }]);
 });
 
 app.listen(3000);
+module.exports.app = app;
