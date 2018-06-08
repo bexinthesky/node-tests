e5427e5e80ec97561530fa7f36a1980e31df5ca0
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-06-08 13:46:11 -0700

    finished coding out how to test spies

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,10 +1,26 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+    var db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__('db', db);
 
     it('should call the spy correctly', () => {
         var spy = expect.createSpy();
-        spy();
-        expect(spy).toHaveBeenCalled();
+        spy('Usagi', 21);
+        expect(spy).toHaveBeenCalledWith('Usagi', 21);
     });
+
+    it('should call saveUser with user object', () => {
+        var email = 'usagi@example.com';
+        var password = '123ABC';
+
+        app.handleSignup(email, password);
+        expect(db.saveUser).toHaveBeenCalledWith({email, password});
+    });
+
 });
